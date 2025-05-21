f4cfbe72e99395f5046d910370cee8b9bf9716ee
Thandazani <thandazanijrc@gmail.com>
2025-05-21 18:14:37 +0200

    Created basic cucumber report

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/Features",
         glue = {"StepsDefinition"},
-        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
+        plugin = {"pretty", "html:Reports/cucumber-reports.html", "json:target/cucumber.json"},
         //monochrome = true,
         tags = "@AddUser"
 )
