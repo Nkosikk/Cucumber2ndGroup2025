aed9d7b382019b0426d72154c11cfc1fd307af83
Nkosi <njceles@gmail.com>
2025-05-21 19:49:16 +0200

    disabled the tag

--- src/test/java/Runner/RunnerTest.java
+++ src/test/java/Runner/RunnerTest.java
@@ -9,7 +9,8 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"pretty", "html:Reports/cucumber-reports.html", "json:target/cucumber.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true,
-        tags = "@AddUser",publish = true
+//        tags = "@AddUser",
+        publish = true
 )
 
 public class RunnerTest extends AbstractTestNGCucumberTests {
