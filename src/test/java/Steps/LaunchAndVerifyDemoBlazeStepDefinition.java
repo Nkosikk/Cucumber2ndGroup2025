9e16b6043d7e1badd16535b068a60973a8e9ea71
mmatjila <mabitselama@gmail.com>
2025-05-25 13:02:48 +0200

    Update feature file

---
+++ src/test/java/Steps/LaunchAndVerifyDemoBlazeStepDefinition.java
@@ -0,0 +1,26 @@
+package Steps;
+
+import io.cucumber.java.en.*;
+
+public class LaunchAndVerifyDemoBlazeStepDefinition {
+
+
+    @Given("I launch website")
+    public void i_launch_website() {
+
+    }
+    @And("I click on the {string} link")
+    public void i_click_on_the_link(String string) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("I verify that I am in the product store")
+    public void i_verify_that_i_am_in_the_product_store() {
+
+    }
+
+
+
+
+
+}
--- src/test/resources/Features/LaunchAndVerifyDemoBlaze.feature
+++ src/test/resources/Features/LaunchAndVerifyDemoBlaze.feature
@@ -2,4 +2,5 @@ Feature: Launch and Verify Product Store
 
   Scenario: Launch the website and verify the product store
     Given I launch website
+    And I click on the "Product Store" link
     Then I verify that I am in the product store
