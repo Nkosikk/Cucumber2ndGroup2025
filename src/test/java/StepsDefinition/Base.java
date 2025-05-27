e5990a1a79dbfd152d2ce783ae381f33e8b23cf9
Precious <modikaprecious@gmail.com>
2025-05-27 18:15:49 +0200

    Added cartpage, and steps for cart

--- src/test/java/StepsDefinition/Base.java
+++ src/test/java/StepsDefinition/Base.java
@@ -9,8 +9,8 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Base {
 
-    static BrowserFactory browserFactory = new BrowserFactory();
-    static final WebDriver driver = browserFactory.startBrowser("chrome", "https://www.demoblaze.com/index.html");
+    BrowserFactory browserFactory = new BrowserFactory();
+    final WebDriver driver = browserFactory.startBrowser("chrome", "https://www.demoblaze.com/index.html");
     ProductPage productPage = PageFactory.initElements(driver, ProductPage.class);
     CartPage cartPage = PageFactory.initElements(driver, CartPage.class);
 
