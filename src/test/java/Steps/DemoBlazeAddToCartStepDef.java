40945476d10e4f7ec13b1b25bf42671c818babb6
mmatjila <mabitselama@gmail.com>
2025-05-27 20:58:57 +0200

    2nd step def complete

--- src/test/java/Steps/DemoBlazeAddToCartStepDef.java
+++ src/test/java/Steps/DemoBlazeAddToCartStepDef.java
@@ -4,9 +4,13 @@ import io.cucumber.java.en.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Alert;
+
 
 public class DemoBlazeAddToCartStepDef {
-
+   // Declare driver as a class-level variable
+    WebDriver driver;
 
     @Given("I click {string} under categories")
     public void i_click_under_categories(String string) {
@@ -16,27 +20,35 @@ public class DemoBlazeAddToCartStepDef {
         driver.manage().window().maximize();
         driver.findElement(By.linkText(string)).click();
     }
+
     @When("I click on any laptop of my choice")
     public void i_click_on_any_laptop_of_my_choice() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        // Locate the first laptop in the list and click on it
+        driver.findElement(By.cssSelector(".hrefch")).click();
+
     }
+
     @When("I add the laptop to the cart")
     public void i_add_the_laptop_to_the_cart() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        // Locate and click the "Add to cart" button
+        driver.findElement(By.cssSelector(".btn-success")).click();
     }
+
     @Then("I verify the message on the popup {string}")
-    public void i_verify_the_message_on_the_popup(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_verify_the_message_on_the_popup(String expectedMessage) {
+        // Switch to the alert popup
+        Alert alert = driver.switchTo().alert();
+        String actualMessage = alert.getText();
+
+        // Verify the popup message
+        assert actualMessage.equals(expectedMessage) : "Expected message: " + expectedMessage + ", but got: " + actualMessage;
     }
+
     @Then("I click the OK button")
     public void i_click_the_ok_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        // Accept the alert to close the popup
+
+
     }
-
-
 
 }
